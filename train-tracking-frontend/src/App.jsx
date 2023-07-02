5bbe00228dead6d472770d82630a5947753726c2
Pasindu Eranga <mr.h.d.pasindueranga@gmail.com>
2023-07-02 21:07:42 +0530

    add react router navigation dummy

--- train-tracking-frontend/src/App.jsx
+++ train-tracking-frontend/src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      {/* <Router>
+        <Routes>
+          <Route path="/page/login" element={<Login />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<Login />} />
+            <Route path="/page/landing" element={<Landing />} />
+            <Route path="/page/store" element={<Store />} />
+            <Route path="/page/account" element={<Account />} />
+            <Route path="/page/shuttle" element={<Shuttle />} />
+            <Route path="/page/schedule" element={<Schedule />} />
+            <Route path="/page/inbox" element={<Inbox />} />
+            <Route path="/page/unauthorized" element={<Unauthorized />} />
+            <Route
+              path="page/unauthorized/access"
+              element={<UnauthorizedAccess />}
+            />
+          </Route>
+        </Routes>
+      </Router> */}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +48,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
