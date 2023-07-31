c4d78ddd4be6cb6dd7a6ff68f29b41ae5d182166
Pasindu Eranga <mr.h.d.pasindueranga@gmail.com>
2023-07-31 20:21:25 +0530

    add ws chart endpoint

--- train-tracking-backend/src/main/java/com/tracker/tracker/services/IBookingService.java
+++ train-tracking-backend/src/main/java/com/tracker/tracker/services/IBookingService.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface IBookingService {
-    BookingResponse bookingCreate(BookingCreate bookingRequest, Principal principal);
+    BookingResponse  bookingCreate(BookingCreate bookingRequest, Principal principal);
 
     BookingResponse updateBooking(UUID id, BookingCreate bookingRequest, Principal principal);
 
