fa77440edd7b189ef940d82d3009bf5daae57e1f
supun9915 <supunsandaruwan543@gmail.com>
2023-08-08 01:20:36 +0530

    admin page

--- train-admin-frontend/src/auth/Login.jsx
+++ train-admin-frontend/src/auth/Login.jsx
@@ -6,8 +6,11 @@ import { useDispatch } from "react-redux";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { setCredentials } from "../redux/features/authSlice";
 import { POST, request } from "../api/ApiAdapter";
+import logo from "../assets/images/logo-img/train1.jpg";
+
 // import { toast } from 'react-toastify';
 // import { useAuth } from "../context/authContext";
+import "./Login.css";
 
 const Login = () => {
   // const { setAuth } = useAuth();
@@ -52,76 +55,88 @@ const Login = () => {
   }, []);
 
   return (
-    <div className="flex w-screen h-screen">
-      <div className="signin-left flex justify-center items-center px-20">
-        ttt
-      </div>
+    <div
+      style={{
+        backgroundImage: `url(${logo})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+      className="flex w-screen h-screen signin-left flex justify-center items-center px-20 bg"
+    >
+      <div className="signin-left flex justify-center items-center px-20 "></div>
       <div className="signin-right px-20 flex justify-center items-center">
-        <div className="signin-login-content">
-          <div className="signin-login-top">
-            <div className="signin-user-icon mt-8">
-              <Fa.FaUserCircle color="gray" size="5em" />
+        <div
+          className="signin-login-content"
+          style={{ backgroundColor: "rgba(3, 3, 19, 0.7)" }}
+        >
+          <div className="mt-36">
+            <div className="signin-login-top">
+              <div className="signin-user-icon mt-8">
+                <Fa.FaUserCircle color="white" size="5em" />
+              </div>
+              <div className="signin-login-heading mt-6 text-white">
+                Shuttle Tracking
+              </div>
+              <div className="text-sm font-normal text-white">
+                Login To Your Account
+              </div>
             </div>
-            <div className="signin-login-heading mt-6">Shuttle Tracking</div>
-            <div className="text-sm font-normal text-gray-600">
-              Login To Your Account
-            </div>
-          </div>
-          <div className="signin-login-body mt-6 px-10 space-y-4">
-            <div className="signin-section flex flex-col">
-              <label
-                htmlFor="username"
-                className="text-gray-600 text-sm mb-1 font-semibold select-none"
-              >
-                Username
-              </label>
-              <input
-                id="username"
-                name="username"
-                onChange={(e) => onChange(e)}
-                value={input.username}
-                placeholder="Enter Username"
-                type="text"
-                className="bg-gray-100 px-4 py-2 rounded-lg shadow-md text-sm border-[1px] border-gray-300"
-              />
-            </div>
-            <div className="signin-section relative flex flex-col">
-              <label
-                htmlFor="password"
-                className="text-gray-600 text-sm mb-1 font-semibold select-none"
-              >
-                Password
-              </label>
-              <input
-                id="password"
-                name="password"
-                value={input.password}
-                onChange={(e) => onChange(e)}
-                onKeyPress={(e) => handleEnterKeypress(e)}
-                placeholder="Enter Password"
-                type={showPassword ? "text" : "password"}
-                className="bg-gray-100 px-4 py-2 rounded-lg shadow-md text-sm border-[1px] border-gray-300"
-              />
-              <div className="absolute buttom top-9 right-3">
-                {showPassword ? (
-                  <FaEye
-                    className="cursor-pointer"
-                    onClick={() => setShowPassword(false)}
-                  />
-                ) : (
-                  <FaEyeSlash
-                    className="cursor-pointer"
-                    onClick={() => setShowPassword(true)}
-                  />
-                )}
+            <div className="signin-login-body mt-6 px-10 space-y-4">
+              <div className="signin-section flex flex-col pl-28 pr-28 p-2">
+                <label
+                  htmlFor="username"
+                  className="text-white text-sm mb-1 font-semibold select-none"
+                >
+                  Username
+                </label>
+                <input
+                  id="username"
+                  name="username"
+                  onChange={(e) => onChange(e)}
+                  value={input.username}
+                  placeholder="Enter Username"
+                  type="text"
+                  className="bg-gray-100 px-4 py-2 rounded-lg shadow-md text-sm border-[1px] border-gray-300"
+                />
+              </div>
+              <div className="signin-section relative flex flex-col pl-28 pr-28 p-2">
+                <label
+                  htmlFor="password"
+                  className="text-white text-sm mb-1 font-semibold select-none"
+                >
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  value={input.password}
+                  onChange={(e) => onChange(e)}
+                  onKeyPress={(e) => handleEnterKeypress(e)}
+                  placeholder="Enter Password"
+                  type={showPassword ? "text" : "password"}
+                  className="bg-gray-100 px-4 py-2 rounded-lg shadow-md text-sm border-[1px] border-gray-300"
+                />
+                <div className="absolute buttom top-9 right-3">
+                  {showPassword ? (
+                    <FaEye
+                      className="cursor-pointer"
+                      onClick={() => setShowPassword(false)}
+                    />
+                  ) : (
+                    <FaEyeSlash
+                      className="cursor-pointer"
+                      onClick={() => setShowPassword(true)}
+                    />
+                  )}
+                </div>
               </div>
             </div>
           </div>
-          <div className="mt-10 flex justify-center">
+          <div className="mt-16 flex justify-center">
             <button
               type="button"
               onClick={() => login()}
-              className="px-10 py-2 bg-blue-600 text-white rounded-lg shadow-lg select-none"
+              className="px-10 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-lg shadow-lg select-none"
             >
               Sign In
             </button>
